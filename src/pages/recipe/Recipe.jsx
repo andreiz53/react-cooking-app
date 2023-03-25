69c51328cb8a85abb3127a2bf36a001f255b51c6
andreiz53 <andrei.zamfira53@gmail.com>
2023-03-25 13:35:48 +0200

    created recipe individual page

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
 
 function Recipe() {
-  return <div>Recipe</div>;
+  let { id } = useParams();
+  const url = `http://localhost:3000/recipes/${id}`;
+  const { data: recipe, isPending, error } = useFetch(url);
+
+  return (
+    <div className='recipe'>
+      {error && <p>{error}</p>}
+      {isPending && <p>Loading...</p>}
+      {recipe && (
+        <>
+          <h2>{recipe.title}</h2>
+          <p className='cooking-time'>Takes {recipe.cookingTime} to make</p>
+          <h4>Ingredients:</h4>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <h4>Process:</h4>
+          <p className='process'>{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Recipe;
